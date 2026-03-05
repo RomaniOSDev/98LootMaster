3b252c3c49f93ca8e2fd1dc470485089268a7673
Roman <romazan.26@gmail.com>
2026-03-05 13:36:25 +0300

    Initial Commit

---
+++ 98LootMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  98LootMaster
+//
+//  Created by Роман Главацкий on 05.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
